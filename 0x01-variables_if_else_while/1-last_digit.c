e423e99b9dd49ba48874c15d0ffbf824dab916b4
Joachimm <joachimokoh23@gmail.com>
2023-03-17 17:46:49 +0100

    My second alx task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int ld = (n%10);
+
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if (ld < 6 && ld != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	return (0);
+}
