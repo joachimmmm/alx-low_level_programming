7274bbb3cfcfd2c5c31f266059b471eb35a7b8d7
Joachimm <joachimokoh23@gmail.com>
2023-03-21 17:52:01 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@ void print_alphabet(void)
 {
         int alphabet;
 
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
         {
                 putchar(tolower(alphabet));
         }
