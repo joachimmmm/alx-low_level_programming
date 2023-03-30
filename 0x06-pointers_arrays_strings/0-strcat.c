285083581da2f77718ce2081869335c8974bb359
Joachimm <joachimokoh23@gmail.com>
2023-03-30 02:48:13 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,9 @@ char *_strcat(char *dest, char *src)
 		dlen++;
 	}
 
-	for (i = 0; src[1] != 0; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
-		dest[dlen] = src[1];
+		dest[dlen] = src[i];
 		dlen++;
 	}
 	dest[dlen] = '\0';
