66426951df1fcc8917b9dfea25664bd05a22d64a
Joachimm <joachimokoh23@gmail.com>
2023-04-04 03:55:58 +0100

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,11 +14,11 @@ void print_diagsums(int *a, int size)
 	int num_1;
 	int num_2;
 
-	for (y = 0; y <= ((size) * (size)); y = y + size + 1)
+	for (y = 0; y <= (size * size); y = y + size + 1)
 	{
 		num_1 = num_1 + a[y];
 	}
-	for (z = size - 1; z <= ((size) * (size)); z = z + size - 1)
+	for (z = size - 1; z <= (size * size) - size; z = z + size - 1)
 	{
 		num_2 = num_2 + a[z];
 	}
