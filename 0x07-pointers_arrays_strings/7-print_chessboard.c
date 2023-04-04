f121323baf5458c5c025dc66ea3cd62be81e5d6a
Joachimm <joachimokoh23@gmail.com>
2023-04-04 03:29:21 +0100

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,13 +11,13 @@ void print_chessboard(char (*a)[8])
 	int y;
 	int z;
 
-	for (y = 0; y < 0; y++)
+	for (y = 0; y < 8; y++)
 	{
-		for (z = 0; z < 0; z++)
+		for (z = 0; z < 8; z++)
 		{
 			putchar(a[y][z]);
-			putchar('\n');
 		}
+		putchar('\n');
 	}
 }
 
