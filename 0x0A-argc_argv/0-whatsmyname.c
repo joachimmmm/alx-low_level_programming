1b1e044a2aaa0cd863f86c5138bbb71692050e5d
Joachimm <joachimokoh23@gmail.com>
2023-04-07 14:59:07 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,15 @@
 
 /**
  * main - entry point
+ * @argc: counts the num of string in cl
+ * @argv: array that holds str vl entered in cl
  * Return: always 0(success)
  */
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
