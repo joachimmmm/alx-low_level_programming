a591d3482f9b758add3f7ceeb37db812b95c6e32
Joachimm <joachimokoh23@gmail.com>
2023-04-07 15:14:28 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: variable that shows num of args
+ * @argv: arr
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
