c83c7b5d6396d2821395ab853c40b4e468b79b8e
Joachimm <joachimokoh23@gmail.com>
2023-04-17 21:27:34 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
+/**
+ * struct dog - details of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog.
+ *
+ * Description: it is the primary details of a dog
+ */
 
 struct dog
 {
