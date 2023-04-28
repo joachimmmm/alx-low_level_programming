64a6b375230a90e320c92600b472d22b8f4fed9d
Joachimm <joachimokoh23@gmail.com>
2023-04-28 02:55:11 +0100

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: headnode
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *t;
+
+	while (head)
+	{
+		t = (*head).next;
+		free((*head).str);
+		free(head);
+		head = t;
+	}
+}
