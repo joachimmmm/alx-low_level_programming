58b8d988c7be2587c0144055384d41187c9bf868
Joachimm <joachimokoh23@gmail.com>
2023-05-03 01:01:25 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *t_n;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	while (*head)
 	{
 		t_n = (**head).next;
 		free(*head);
 		*head = t_n;
-	}
-	if (head == NULL)
-	{
-		return;
 	}
 	*head = NULL;
 }
