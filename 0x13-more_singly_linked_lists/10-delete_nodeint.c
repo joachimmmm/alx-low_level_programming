d87e5ac53eafb1aee2bfc3b6b7626a7cf6160b32
Joachimm <joachimokoh23@gmail.com>
2023-05-03 04:52:09 +0100

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_node_at_index - deletes node at index of l.list
+ * delete_nodeint_at_index - deletes node at index of l.list
  * @head: head node
  * @index: node index
  * Return: 1(success), -1(failed)
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tempNode);
 		return (1);
 	}
-	while (idx < (index -1))
+	while (idx < (index - 1))
 	{
 		if ((!(*tempNode).next) || !tempNode)
 		{
