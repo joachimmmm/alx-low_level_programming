26e14c470365ca5446b720fef5d9433e17560c7f
Joachimm <joachimokoh23@gmail.com>
2023-05-09 01:45:19 +0100

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of file.
+ * @text_content: texts of file content
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int filep;
+	int index;
+	int writer;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		for (index = 0; text_content[index];)
+		{
+			index++;
+		}
+	}
+	filep = open(filename, O_CREAT | O_TRUNC | O_RDWR);
+	writer = write(filep, text_content, index);
+	if (writer == -1 || filep == -1)
+	{
+		return (-1);
+	}
+	close(filep);
+	return (1);
+}
